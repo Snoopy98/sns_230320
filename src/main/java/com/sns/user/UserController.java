aa62ba28e1707ad91a6e6c2620a18aace1988c6e
Test <snoopy980306>
2023-07-14 18:43:51 +0900

    no message

---
+++ src/main/java/com/sns/user/UserController.java
@@ -0,0 +1,14 @@
+package com.sns.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@GetMapping("/sign_up_view")
+	public String signUpView() {
+		return "template/layout";
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/include/header.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<div class="d-flex align-items-center justify-content-between">
+	<div>
+		<h1>Marondalgram</h1>
+	</div>
+	<div>
+		<span>marobiana 님 안녕하세요!</span>
+		<a href="#">로그아웃</a>
+	</div>
+</div>
